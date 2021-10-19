139301a1404598d0ccfce6f42286cc11404c5ca2
Dmytro Zinkevych <dzinkevych@gmail.com>
2021-10-19 20:14:19 +0300

    Initial commit

---
+++ GetLinks.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class GetLinks {
+    public static void main(String[] args) {
+        final String FILE_EXTENSION = ".url";
+        final String URL_LINE_START = "URL=";
+        final String DIRECTORY = "./";
+        try (
+                Stream<Path> stream = Files.list(Paths.get(DIRECTORY));
+        ) {
+            String links = stream
+                    .filter(filePath -> !Files.isDirectory(filePath)
+                            && filePath.getFileName().toString().toLowerCase().endsWith(FILE_EXTENSION))
+                    .map(filePath -> {
+                        try {
+                            String fileContent = Files.readString(filePath);
+                            int urlStartIndex = fileContent.lastIndexOf(URL_LINE_START);
+                            if (fileContent.contains("[InternetShortcut]") && urlStartIndex >= 0) {
+                                String fileName = filePath.getFileName().toString();
+                                return fileName.substring(0, fileName.length() - FILE_EXTENSION.length())
+                                        + "\n"
+                                        + fileContent.substring(urlStartIndex + URL_LINE_START.length()).stripTrailing();
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        return "";
+                    })
+                    .collect(Collectors.joining("\n\n"));
+            Files.writeString(Files.createFile(Paths.get(DIRECTORY + "links.txt")), links);
+            System.out.println("Links saved successfully :)");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ GetLinksJava8.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class GetLinksJava8 {
+    public static void main(String[] args) {
+        final String FILE_EXTENSION = ".url";
+        final String URL_LINE_START = "URL=";
+        try (
+                Stream<Path> stream = Files.list(Paths.get("./"));
+                BufferedWriter writer = new BufferedWriter(new FileWriter("links.txt"))
+        ) {
+            stream
+                    .filter(filePath -> !Files.isDirectory(filePath)
+                            && filePath.getFileName().toString().toLowerCase().endsWith(FILE_EXTENSION))
+                    .map(Path::toFile)
+                    .forEach(file -> {
+                        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+                            String line = br.readLine();
+                            if (line.contains("[InternetShortcut]")) {
+                                line = br.readLine();
+                                if (line.toUpperCase().startsWith(URL_LINE_START)) {
+                                    writer
+                                            .append(file.getName(), 0, file.getName().length() - FILE_EXTENSION.length())
+                                            .append("\n")
+                                            .append(line.substring(URL_LINE_START.length()))
+                                            .append("\n\n");
+                                }
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+            System.out.println("Links saved successfully :)");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
